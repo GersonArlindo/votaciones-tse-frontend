3cfa06fd925cab0f736316c36008cd26df2f5dca
Vilche <carlitosviche@gmail.com>
2023-10-25 20:48:48 -0600

    ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  API_URL: "http://34.222.26.66/api/v1/",
+  API_URL: "http://54.218.81.88/api/v1/",
   API_URL_AUTH: "http://localhost:8080/"
 }
