c35a3c6cbaceb32463baf85a9842bc0cc31a97ac
GersonArlindo <gerson50039@gmail.com>
2023-11-03 18:18:46 -0600

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  API_URL: "http://54.218.81.88/api/v1/",
+  API_URL: "http://34.219.81.120/api/v1/",
   API_URL_AUTH: "http://localhost:8080/"
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 
 export const environment = {
   production: false,
-   API_URL: "http://54.218.81.88/api/v1/",
+   API_URL: "http://34.219.81.120/api/v1/",
    API_URL_AUTH: "http://localhost:8080/" 
 };
 
